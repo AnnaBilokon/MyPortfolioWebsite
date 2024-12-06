597503456a8e10f09d13fb31e2c68f2dfc9eed24
AnnaBilokon <Anna.bilokon89@gmail.com>
2024-12-06 17:28:28 +0100

    add new colors and dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 // /** @type {import('tailwindcss').Config} */
 module.exports = {
+	darkMode: 'class',
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  
   theme: {
-    extend: {},
-  },
+    extend: {	
+		colors: {
+        'primary-light': '#221c2b',
+		'primary-dark': '#f1e9f8',
+  },},
   plugins: [],
+  
 }
 
+}
