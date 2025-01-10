df250ae140c251e71869b986d2da9a4207ca19ff
AnnaBilokon <Anna.bilokon89@gmail.com>
2025-01-10 11:46:16 +0100

    add theme provider

--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@ import AboutSection from './components/AboutSection.jsx';
 import ProjectSection from './components/ProjectSection.jsx';
 import EmailSection from './components/EmailSection.jsx';
 import Footer from './components/Footer.jsx';
+import { ThemeProvider } from './ThemeContext.jsx';
+import SkillsSection from './components/SkillsSection.jsx';
 
 
 export default function Home() {
   return (
-	<main className="flex min-h-screen flex-col bg-[#121212]">
+	<ThemeProvider>
+	<main className="flex min-h-screen flex-col bg-[#ffffff] dark:bg-[#221c2b]">
 	<Navbar/>
 	<div className="container mt-24 mx-auto px-12 py-4">
 	<HeroSection/>
 	<AboutSection/>
+	<SkillsSection/>
 	<ProjectSection/>
 	<EmailSection/>
 	<Footer/>
 	</div>
 	</main>
+	</ThemeProvider>
+	
   )
 }
