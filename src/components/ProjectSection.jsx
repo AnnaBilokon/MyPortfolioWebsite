1653ad880feb21e49eb2b553cdd4ab56145600d7
AnnaBilokon <Anna.bilokon89@gmail.com>
2025-01-14 11:40:03 +0100

    styling the component

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -4,11 +4,12 @@ import { projectsData } from '../projectData'
 
 const ProjectSection = () => {
   return (
-    <section id="projects">
-      <h2 className="text-center text-4xl font-bold text-white mt-8 mb-8 md:mb-12 ">
+    <section id="projects" className="mt-28">
+      <h2 className="text-center text-4xl font-bold text-primary-light dark:text-primary-dark mt-8 mb-8 md:mb-12 ">
         My projects
       </h2>
-      <div className="grid xl:grid-cols-3 justify-items-center gap-8 md:gap-12 ">
+      {/* grid xl:grid-cols-3 justify-items-center gap-8 md:gap-12 */}
+      <div className="grid xl:grid-cols-2 justify-items-center gap-8 md:gap-12 ">
         {projectsData.map((project) => (
           <ProjectCard
             key={project.id}
@@ -17,6 +18,7 @@ const ProjectSection = () => {
             imgUrl={project.image}
             gitUrl={project.gitUrl}
             previewUrl={project.previewUrl}
+            labels={project.labels}
           />
         ))}
       </div>
