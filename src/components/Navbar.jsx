c4c9a8e5102011d3f77ff3f283a16ea034690b66
AnnaBilokon <Anna.bilokon89@gmail.com>
2025-01-14 11:39:49 +0100

    mode switch button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import NavLink from '../components/Navlinks'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
 import MenuOverlay from './MenuOverlay'
-import logoImage from '../images/logo.png'
+import ThemeContext from '../ThemeContext.jsx'
+import { PiCodeDuotone } from 'react-icons/pi'
+import { IconContext } from 'react-icons'
 
 const navLinks = [
   {
@@ -22,37 +24,39 @@ const navLinks = [
 
 const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false)
-
+  const { theme, toggleTheme } = useContext(ThemeContext)
   return (
-    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
-      <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
-        <a
-          rel="preload"
-          href="/"
-          className="text-2xl md:text-5xl text-white font-semibold"
+    <nav className="flex flex-row justify-between fixed mx-auto py-3 top-0 left-0 right-0 z-10 bg-opacity-50 backdrop-blur-lg">
+      <a
+        rel="preload"
+        href="/"
+        className="text-2xl md:text-5xl font-semibold mx-8"
+      >
+        {' '}
+        <IconContext.Provider
+          value={{
+            color: '#a275d9',
+            className: 'global-class-name',
+            size: '4rem',
+          }}
         >
           {' '}
-          <img
-            src={logoImage}
-            color="white"
-            width="60"
-            height="60"
-            alt="logo"
-          />
-        </a>
-
+          <PiCodeDuotone />
+        </IconContext.Provider>
+      </a>
+      <div className="flex containe lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
         <div className="mobile-menu block md:hidden ">
           {!navbarOpen ? (
             <button
               onClick={() => setNavbarOpen(true)}
-              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+              className="flex items-center px-3 py-2 border rounded border-slate-200 dark:text-primary-dark hover:text-white hover:border-white"
             >
               <Bars3Icon className="h-5 w-5" />
             </button>
           ) : (
             <button
               onClick={() => setNavbarOpen(false)}
-              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+              className="flex items-center px-3 py-2 border rounded border-slate-200 hover:text-white hover:border-white"
             >
               <XMarkIcon className="h-5 w-5" />
             </button>
@@ -70,6 +74,12 @@ const Navbar = () => {
         </div>
       </div>
       {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
+      <button
+        onClick={toggleTheme}
+        className="w-fit h-10 py-2 px-4 mx-5 my-auto rounded-full font-semibold text-sm border-2 border-primary-light hover:scale-105 transition-all duration-300 dark:border-primary-dark dark:text-primary-dark"
+      >
+        {theme === 'dark' ? 'Light Mode' : 'Dark Mode'}
+      </button>
     </nav>
   )
 }
