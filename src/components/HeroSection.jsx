9ec4cda06cb3c4182e57df6269919e6e004754b4
AnnaBilokon <Anna.bilokon89@gmail.com>
2025-08-08 13:36:02 +0200

    spelling fix

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -59,7 +59,7 @@ const HeroSection = () => {
             Django, REST APIs, Docker, and Azure, aiming to grow into a
             Full-stack Developer. I'm driven by curiosity and a strong empathy
             for users. I stay updated on industry standards and best practices
-            to enhance user experience. By combining software engineering with
+            to enhance user experience by combining software engineering with
             user experience design.
           </p>
 
