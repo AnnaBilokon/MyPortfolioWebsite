639452d93d0763dcf5da5fb27f4187533ef25695
AnnaBilokon <Anna.bilokon89@gmail.com>
2025-08-08 18:41:17 +0200

    update the card component (condition for preview button)

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -61,14 +61,16 @@ const ProjectCard = ({
               >
                 <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
               </a>
-              <a
-                href={previewUrl}
-                rel="noreferrer"
-                target="_blank"
-                className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
-              >
-                <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
-              </a>
+              {previewUrl && (
+                <a
+                  href={previewUrl}
+                  rel="noreferrer"
+                  target="_blank"
+                  className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
+                >
+                  <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
+                </a>
+              )}
             </div>
           </div>
 
